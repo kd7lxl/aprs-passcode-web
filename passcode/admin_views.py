e243c9a22f1acda89ed6bee705b9acc7311c4642
Tom Hayward <tom@harbortronics.com>
2010-08-22 11:23:08 -0700

    approve/deny: redirect to referer.

--- passcode/admin_views.py
+++ passcode/admin_views.py
@@ -6,10 +6,12 @@ def approve(request, passcode_request_id):
     passcode_request = PasscodeRequest.objects.get(id=passcode_request_id)
     passcode_request.action_by = request.user
     passcode_request.approve()
-    return HttpResponseRedirect('/admin/passcode/passcoderequest/')
+    return HttpResponseRedirect(request.META['HTTP_REFERER'] if request.META['HTTP_REFERER'] != '' else '/admin/passcode/passcoderequest/')
+approve = staff_member_required(approve)
 
 def deny(request, passcode_request_id):
     passcode_request = PasscodeRequest.objects.get(id=passcode_request_id)
     passcode_request.action_by = request.user
     passcode_request.deny()
-    return HttpResponseRedirect('/admin/passcode/passcoderequest/')
+    return HttpResponseRedirect(request.META['HTTP_REFERER'] if request.META['HTTP_REFERER'] != '' else '/admin/passcode/passcoderequest/')
+deny = staff_member_required(deny)
